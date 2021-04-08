51cd91bf58866c94cb1ac608ea082269991b0a5a
Irfan <irfanali987654@gmail.com>
2021-04-09 00:45:59 +0530

    password security with bcrypt

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 //jshint esversion:6
+require('dotenv').config()
 const express = require("express");
 const mongoose = require("mongoose")
 const bodyparser = require("body-parser");
 const ejs = require("ejs");
-const encrypt = require("mongoose-encryption")
+// const encrypt = require("mongoose-encryption")
+// const md5=require("md5")
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
 
 const app=express();
@@ -13,14 +17,15 @@ app.set("view engine","ejs");
 
 app.use(bodyparser.urlencoded({extended:true}));
 
-mongoose.connect("mongodb://localhost:27017/users",{useNewUrlParser:true,useUnifiedTopology:true})
+mongoose.connect(process.env.DB_HOST,{useNewUrlParser:true,useUnifiedTopology:true})
 
 const userSchema = new mongoose.Schema({
     email:String,
     password:String
 });
-const key = "Thisismyfirstencryptkeywithauthentication"
-userSchema.plugin(encrypt, { secret:key,encryptedFields: ['password'] });
+
+// userSchema.plugin(encrypt, { secret:process.env.KEY,encryptedFields: ['password'] });
+
 const User = mongoose.model("User",userSchema);
 
 app.get("/",(req,res)=>{
@@ -33,18 +38,21 @@ app.get("/register",(req,res)=>{
     res.render("register")
 })
 app.post("/register",(req,res)=>{
-    const newUser=  new User({
-        email:req.body.username,
-        password:req.body.password
-    })
-    newUser.save((err)=>{
-        if(err){
-            console.log(err)
-        }
-        else{
-            res.render("secrets")
-        }
+    bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+        const newUser=  new User({
+            email:req.body.username,
+            password:hash
+        })
+        newUser.save((err)=>{
+            if(err){
+                console.log(err)
+            }
+            else{
+                res.render("secrets")
+            }
+        });
     });
+   
 })
 
 app.post("/login",(req,res)=>{
@@ -52,9 +60,13 @@ app.post("/login",(req,res)=>{
          if(err){
              console.log(err)
          }else{
-             if(user.password===req.body.password){
-                 res.render("secrets")
-             }
+            bcrypt.compare(req.body.password, user.password, function(err, result) {
+                if(result===true){
+                    res.render("secrets")
+                }
+                
+            });
+            
          }
      })
 })
